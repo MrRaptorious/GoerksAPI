e0869cd87bea21746346a60e01e7641918d4e8a0
MrRaptorious <Lorenz.schmitten@gmail.com>
2020-09-01 16:06:41 +0200

    Setup EFcore

--- GoerksAPI/Startup.cs
+++ GoerksAPI/Startup.cs
@@ -10,6 +10,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using GoerksAPI.Models;
+using Microsoft.EntityFrameworkCore.Storage;
 
 namespace GoerksAPI
 {
@@ -25,7 +28,17 @@ namespace GoerksAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContext<TodoContext>(opt =>
+            //    opt.UseInMemoryDatabase("TodoList"));
+
+            services.AddDbContext<TodoContext>(opt =>
+                opt.UseSqlite("Filename=MyDatabase.db"));
+
+
+
             services.AddControllers();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
