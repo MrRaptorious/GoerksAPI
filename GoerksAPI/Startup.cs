1a5ebc90f61c7adcf2fc619034fa5bb39fe9960c
MrRaptorious <Lorenz.schmitten@gmail.com>
2020-12-08 15:36:22 +0100

    bugfix

--- GoerksAPI/Startup.cs
+++ GoerksAPI/Startup.cs
@@ -22,15 +22,12 @@ namespace GoerksAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers().AddNewtonsoftJson();
+            services.AddControllers().AddNewtonsoftJson(); 
             services.AddTokenAuthentication(Configuration);
-
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -38,7 +35,7 @@ namespace GoerksAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
             app.UseAuthentication();
